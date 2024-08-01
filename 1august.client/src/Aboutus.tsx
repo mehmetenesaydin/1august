4b6b1491c5d89ba26008f1d98fdc7cd698169264
Mehmet Enes Aydın <mehmetenesaydinn@gmail.com>
2024-08-01 17:36:05 +0300

    npm install axios npm install react-router-dom
    bileşenlerinin indirilmesi gerekebilir.

---
+++ 1august.client/src/Aboutus.tsx
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="top-section">
+                <button className="button">Giriş Yap</button>
+                <button className="button2">Kaydol</button>
+            </div>
+            <div className="bottom-section">
+                <div className="button-row">
+                    <button className="button">Anasayfa</button>
+                    <button className="button">Sözleşme Seçenekleri</button>
+                    <button className="button">Danışmanlık & Destek</button>
+                    <button className="button">Hakkımızda</button>
+                    <button className="button">İletişim</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
