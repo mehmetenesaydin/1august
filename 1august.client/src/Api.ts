3457efb0571f0d7373aabaa4f23fd5bd2fa15cca
Mehmet Enes Aydın <mehmetenesaydinn@gmail.com>
2024-08-07 09:49:23 +0300

    Son çalışan hali.

--- 1august.client/src/Api.ts
+++ 1august.client/src/Api.ts
@@ -1,13 +1,14 @@
+ï»¿/* eslint-disable @typescript-eslint/no-explicit-any */
 // src/api.ts
 import axios from 'axios';
 
-// Tam API URL'inizi burada tanýmlayýn
-const API_URL = 'http://localhost:5000/api'; // API'nin tam URL'i
+// API URL'inizi burada tanÄ±mlayÄ±n
+const API_URL = 'https://localhost:7005'; // API'nin temel URL'i
 
 export const getContractPdf = async (contractData: any): Promise<Uint8Array> => {
     try {
         const response = await axios.post(`${API_URL}/contract/OnlinePlatformServiceAgreement`, contractData, {
-            responseType: 'arraybuffer' // PDF dosyasýný almak için gerekli
+            responseType: 'arraybuffer' // PDF dosyasÄ±nÄ± almak iÃ§in gerekli
         });
         return new Uint8Array(response.data);
     } catch (error) {
